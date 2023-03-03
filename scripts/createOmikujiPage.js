14d897d95c096b7db36bf3f4eb731c8aebce1489
raamen-lover <eib1yv4eveit6@yahoo.co.jp>
2023-03-03 22:47:25 +0900

    createOmikujiPage関数の重複を解消,CSS微調整

--- scripts/createOmikujiPage.js
+++ scripts/createOmikujiPage.js
@@ -1,36 +1,42 @@
 function createOmikujiPage(omikuji){
     const container = document.createElement('div');
-    container.classList.add('p-3');
+    container.classList.add('p-3','box-width');
 
     const omikujiFrame = document.createElement('div');
     omikujiFrame.classList.add('bg-white','d-flex','justify-content-center','align-items-center','flex-column','my-2');
-
+    var oFortune = "あなたの未来は"+omikuji.fortune;
+    var oLuckyAction = "ラッキーな行動は"+omikuji.luckyAction;
+    var oLuckyShortcut = "ラッキーショートカットは"+omikuji.luckyShortcut;
+    var oLuckyReview = "ラッキーリビューは"+omikuji.luckyReview;
     omikujiFrame.innerHTML = `
     <div class="border border-danger ">
         <div class="pt-3">
-            <h4 class="text-danger text-center yuji-syuku">おみくじ</h4>
+            <h4 class="text-danger text-center yuji-syuku">ソフトウェア転職みくじ</h4>
         </div>
-        <div class="border-top border-bottom border-danger pt-3">
-            <img src="images/${omikuji.fileNameOfImage}">
-            <h5 class="text-danger text-center yuji-syuku">${omikuji.fortune}</h5>
+        <div class="text-center border-top  border-danger pt-3">
+            <img src="images/${omikuji.fileNameOfImage}" class="omikuji-img">
+            <h3 class="text-danger text-center yuji-syuku">${omikuji.fortune}</h3>
         </div>
-        <div class="border-top border-bottom border-danger pt-3">
-            <p class="text-danger text-center yuji-syuku">${omikuji.description}</p>
+        <div class="border-top  border-danger pt-3">
+            <h5 class="text-danger text-center yuji-syuku">${omikuji.description}</h5>
         </div>
-        <div class="border-top border-bottom border-danger pt-3">
-            <p class="text-danger text-center yuji-syuku">ラッキーな行動</p>
+        <div class="border-top  border-danger pt-3">
+            <h3 class="text-danger text-center yuji-syuku">ラッキーな行動</h3>
             <h5 class="text-danger text-center yuji-syuku">${omikuji.luckyAction}</h5>
         </div>
-        <div class="border-top border-bottom border-danger pt-3">
-            <p class="text-danger text-center yuji-syuku">ラッキーショートカット</p>
+        <div class="border-top  border-danger pt-3">
+            <h3 class="text-danger text-center yuji-syuku">ラッキーショートカット</h3>
             <h5 class="text-danger text-center yuji-syuku">${omikuji.luckyShortcut}</h5>
         </div>
-        <div class="border-top border-bottom border-danger pt-3">
-            <p class="text-danger text-center yuji-syuku">ラッキーリビュー</p>
+        <div class="border-top  border-danger pt-3">
+            <h3 class="text-danger text-center yuji-syuku">ラッキーリビュー</h3>
             <h5 class="text-danger text-center yuji-syuku">${omikuji.luckyReview}</h5>
         </div>
-        <div class="py-3 text-center align-items-center">
-            <button id="re-draw-omikuji" type="button" class="btn btn-danger rounded yuji-syuku">もう一度おみくじを引く</button>
+        <div class="border-top border-danger text-center align-items-center py-3">
+            <button id="re-draw-omikuji" type="button" class="btn btn-danger rounded-pill">もう一度おみくじを引く</button>
+            <button onclick="location.href='https://twitter.com/share?ref_src=twsrc%5Etfw%0a&text=皆もやってみよう!%0a&url=https://github.com/recursion-blue-teamdev/Dev_Omikuji%0a&hashtags=Recursion'" type="button" class="btn btn-danger rounded-pill">Twitterでシェア</a>
+<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+            </button>
         </div>
     </div>
     `;
--- scripts/main.js
+++ scripts/main.js
@@ -38,55 +38,6 @@ function drawOmikuji(){
     let omikuji = new Omikuji();
     goToOmikujiPage(omikuji);
 }
-  
-function createOmikujiPage(omikuji){
-    const container = document.createElement('div');
-    container.classList.add('p-3','box-width');
-
-    const omikujiFrame = document.createElement('div');
-    omikujiFrame.classList.add('bg-white','d-flex','justify-content-center','align-items-center','flex-column','my-2');
-    var oFortune = "あなたの未来は"+omikuji.fortune;
-    var oLuckyAction = "ラッキーな行動は"+omikuji.luckyAction;
-    var oLuckyShortcut = "ラッキーショートカットは"+omikuji.luckyShortcut;
-    var oLuckyReview = "ラッキーリビューは"+omikuji.luckyReview;
-    omikujiFrame.innerHTML = `
-    <div class="border border-danger ">
-        <div class="pt-3">
-            <h4 class="text-danger text-center yuji-syuku">ソフトウェア転職みくじ</h4>
-        </div>
-        <div class="text-center border-top  border-danger pt-3">
-            <img src="images/${omikuji.fileNameOfImage}" class="omikuji-img">
-            <h3 class="text-danger text-center yuji-syuku">${omikuji.fortune}</h3>
-        </div>
-        <div class="border-top  border-danger pt-3">
-            <h5 class="text-danger text-center yuji-syuku">${omikuji.description}</h5>
-        </div>
-        <div class="border-top  border-danger pt-3">
-            <h3 class="text-danger text-center yuji-syuku">ラッキーな行動</h3>
-            <h5 class="text-danger text-center yuji-syuku">${omikuji.luckyAction}</h5>
-        </div>
-        <div class="border-top  border-danger pt-3">
-            <h3 class="text-danger text-center yuji-syuku">ラッキーショートカット</h3>
-            <h5 class="text-danger text-center yuji-syuku">${omikuji.luckyShortcut}</h5>
-        </div>
-        <div class="border-top  border-danger pt-3">
-            <h3 class="text-danger text-center yuji-syuku">ラッキーリビュー</h3>
-            <h5 class="text-danger text-center yuji-syuku">${omikuji.luckyReview}</h5>
-        </div>
-        <div class="border-top border-danger text-center align-items-center py-3">
-            <button id="re-draw-omikuji" type="button" class="btn rounded yuji-syuku">もう一度おみくじを引く</button>
-            <button onclick="location.href='https://twitter.com/share?ref_src=twsrc%5Etfw%0a&text=皆もやってみよう!%0a&url=https://github.com/recursion-blue-teamdev/Dev_Omikuji%0a&hashtags=Recursion'" type="button" class="btn rounded yuji-syuku">Twitterでシェア</a>
-<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-            </button>
-        </div>
-    </div>
-    `;
-    omikujiFrame.querySelector('#re-draw-omikuji').addEventListener('click', function(){backToTopPage();});
-
-    container.append(omikujiFrame);
-
-    return container;
-}
 
 
 
